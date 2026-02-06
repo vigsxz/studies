01e13b2c94f5091a9e1863dbd95463d2914e2dc7
Vitor <vigsxz@gmail.com>
2026-02-06 08:52:01 -0300

    first code

--- logic/index.js
+++ logic/index.js
@@ -1,1 +1,12 @@
-console.log("my first code in javascript");
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Type something:",function (text) {
+    console.log(text);
+    rl.close();
+});
+
+
